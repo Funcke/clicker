b76420c7138a47b491e5f0adeaf1a25a21e62d85
Jonas Funcke <40146720150244@litec.ac.at>
2017-10-04 18:06:42 +0200

    Added useless tts feature, just for fun

--- app/src/main/java/com/example/jonas/clicker/MainActivity.java
+++ app/src/main/java/com/example/jonas/clicker/MainActivity.java
@@ -1,21 +1,32 @@
 package com.example.jonas.clicker;
 
+import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements TextToSpeech.OnInitListener{
     public static int counter = 0;
+    public TextToSpeech tts;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tts = new TextToSpeech(this, this);
     }
 
     public void increase(View view) {
         TextView tw = (TextView) findViewById(R.id.textView);
-        this.counter++;
+        if(this.counter == Integer.MAX_VALUE)
+            this.counter = 0;
+        else
+            this.counter++;
         tw.setText(String.valueOf(this.counter));
+        tts.speak("Go!", TextToSpeech.QUEUE_FLUSH, null);
+    }
+
+    public void onInit(int status) {
+        tts.speak("Welcome", TextToSpeech.QUEUE_FLUSH, null);
     }
 }
